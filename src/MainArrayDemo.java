0743b2ead3f77fb0822fab71bcd803c55a547755
yuyanmei-alice <72970084+yuyanmei-alice@users.noreply.github.com>
2021-04-27 10:42:18 +0800

    Update MainArrayDemo.java

--- src/MainArrayDemo.java
+++ src/MainArrayDemo.java
@@ -2,7 +2,7 @@
 读取Java main函数的string数组，通过javac java命令编译并执行类文件
  */
 
-public class MainArray {
+public class MainArrayDemo {
 
     public static void main(String[] args){
         for (int i=0;i<args.length;i++) {
